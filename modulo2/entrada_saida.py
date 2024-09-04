234480743cad1c8437c2be96548ac1b6f5c13a56
Alexandre Pinheiro <alexclemente1985@gmail.com>
2024-09-04 17:39:25 -0300

    Finalização módulo 3 (SQL e Pandas)

--- modulo2/entrada_saida.py
+++ modulo2/entrada_saida.py
@@ -1,27 +1,31 @@
 from pathlib import Path
 
 import pandas as pd
+#Para o caso de falhas de leitura html no Mac
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def entrada_saida():
-    #Parâmetros
+    # Parâmetros
     ##Index_col = permite definir uma coluna como índice
     ##Parse_dates = verifica quais campos podem ser do tipo data e formata da melhor maneira (True | False)
-    df = pd.read_csv(Path.joinpath(Path.cwd().parent, "datasets","exemplo.csv"),sep=",", decimal=".")
+    df = pd.read_csv(Path.joinpath(Path.cwd().parent, "datasets", "exemplo.csv"), sep=",", decimal=".")
 
     print(df)
-    #Para verificar se todos os dados foram considerados como do tipo float para o caso
+    # Para verificar se todos os dados foram considerados como do tipo float para o caso
     print(df.info())
 
-    #Para exportar o conjunto de dados
-    df.to_csv(Path.joinpath(Path.cwd().parent, "datasets","exports","exemplo_exp.csv"),sep=";", decimal=",")
+    # Para exportar o conjunto de dados
+    df.to_csv(Path.joinpath(Path.cwd().parent, "datasets", "exports", "exemplo_exp.csv"), sep=";", decimal=",")
 
-    #Leitura e escrita de arquivo excel
-    df.to_excel(Path.joinpath(Path.cwd().parent, "datasets","exports","exemplo_exp.xlsx"), sheet_name="Planilha1")
-    df_excel = pd.read_excel(Path.joinpath(Path.cwd().parent, "datasets", "exports", "exemplo_exp.xlsx"), sheet_name="Planilha1")
+    # Leitura e escrita de arquivo excel
+    df.to_excel(Path.joinpath(Path.cwd().parent, "datasets", "exports", "exemplo_exp.xlsx"), sheet_name="Planilha1")
+    df_excel = pd.read_excel(Path.joinpath(Path.cwd().parent, "datasets", "exports", "exemplo_exp.xlsx"),
+                             sheet_name="Planilha1")
     print(df_excel)
 
-    #Leitura de HTML
+    # Leitura de HTML
     df_html = pd.read_html('http://www.fdic.gov/bank/individual/failed/banklist.html')
     print(df_html)
 
--- modulo3/sql.py
+++ modulo3/sql.py
@@ -1,18 +1,94 @@
-#Uso do SQL Lite
+# Uso do SQL Lite
+import os
 import sqlite3
 from pathlib import Path
 
 import pandas as pd
+
+
 def sql():
-    #Criando conexão com banco de dados SQLITE
-    database = Path.joinpath(Path.cwd(),"databases","web.db")
+    # Criando conexão com banco de dados SQLITE
+    path = Path.joinpath(Path.cwd(), "databases")
+    # Criando pasta database caso não exista
+    if not path.exists():
+        Path.mkdir(path, exist_ok=True)
+
+    database = Path.joinpath(Path.cwd(), "databases", "web.db")
     print(database)
     conn = sqlite3.connect(database)
-    bd_data = Path.joinpath(Path.cwd(),"datasets","bd_data.csv")
+    bd_data = Path.joinpath(Path.cwd(), "datasets", "bd_data.csv")
     df_data = pd.read_csv(bd_data, index_col=0)
     df_data.index.name = "index_name"
     print(df_data)
 
-    df_data.to_sql('data',conn, index_label='index_name')
+    df_data.to_sql('data', conn, index_label='index_name', if_exists='replace')
+
+    # Criação do cursor para alterar a base de dados
+    c = conn.cursor()
+    # c.execute('CREATE TABLE IF NOT EXISTS products (product_id, product_name, price)')
+    # c.execute('CREATE TABLE products (product_id, product_name, price)')
+    # conn.commit()
+
+    c.execute('DROP TABLE products')
+    # c.execute('DROP TABLE data')
+    conn.commit()
+
+    c.execute('CREATE TABLE products ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT, '
+              '[price] INTEGER)')
+    ##c.execute('CREATE TABLE IF NOT EXISTS products ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT, '
+    #          '[price] INTEGER)')
+
+    # INSERT
+    c.execute('''INSERT INTO products (product_id, product_name, price)
+        VALUES
+        (1, 'Computer', 800),
+        (2, 'Printer', 200),
+        (3, 'Tablet', 300)
+    ''')
+    conn.commit()
+
+    # df_data2 = df_data.iloc[::-2]
+    # df_data2.to_sql('data', conn, if_exists='append')
+
+    # SELECT
+    c.execute('''SELECT * FROM data''')
+    print(c.fetchone())
+    df_fetchall = pd.DataFrame(c.fetchall())
+    print('---')
+    print(df_fetchall)
+
+    c.execute("SELECT * FROM data WHERE A > 200")
+    df_fetchall = pd.DataFrame(c.fetchall())
+    print('---')
+    print(df_fetchall)
+
+    c.execute("SELECT * FROM data WHERE A > 200 AND B > 100")
+    df_fetchall = pd.DataFrame(c.fetchall())
+    print('---')
+    print(df_fetchall)
+
+    c.execute("SELECT A,B,C FROM data WHERE A > 200 AND B > 100")
+    df_fetchall = pd.DataFrame(c.fetchall())
+    print('--- fechall A, B, C')
+    print(df_fetchall)
+
+    query = "SELECT * FROM data"
+    query2 = "SELECT A,B,C FROM data WHERE A > 200 AND B > 100"
+    df_query = pd.read_sql(query, con=conn, index_col='index_name')
+    df_query2 = pd.read_sql(query2, con=conn)
+
+    print("query 1 ------------")
+    print(df_query)
+    print("query 2 ------------")
+    print(df_query2)
+
+    #UPDATE E DELETE
+    c.execute("UPDATE data SET A=298, B=300 WHERE index_name='b'")
+    conn.commit()
+    print(pd.read_sql(query2, con=conn))
+    c.execute("DELETE FROM data WHERE index_name=1")
+    print(pd.read_sql(query, con=conn, index_col='index_name'))
+
+
 
 sql()
