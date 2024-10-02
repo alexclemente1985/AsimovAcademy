10e87f86fafad05140b341f3b34b1dfe323a3035
Alexandre Pinheiro <alexclemente1985@gmail.com>
2024-10-02 17:40:40 -0300

    primeiro layout pt1

--- modulo_2_intro/intro_dash.py
+++ modulo_2_intro/intro_dash.py
@@ -1,4 +1,30 @@
-def intro_dash():
-    print("Teste intro dash python")
+import dash
+import plotly.express as px
+import pandas as pd
+from dash import html, dcc
 
-intro_dash()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+def intro_dash():
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    df = pd.DataFrame({
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+        })
+
+    fig = px.bar(df, x="Fruit", y="Amount",color="City")
+
+    app.layout = html.Div(id="div1",
+                          children=[
+                              html.H1("Hello Dash", id="h1", style={"color":"#FF00FF"}),
+                              html.Div("Dash: Um framework web para Python"),
+                              dcc.Graph(figure=fig, id="Graph1")
+                          ])
+
+    app.run_server(debug=True, port=8052)
+
+
+if __name__ =="__main__":
+    intro_dash()
